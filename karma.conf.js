fc5742581819e660c9cb7e563b7e36d008dd8b22
mstefaniuk <marcin@stefaniuk.info>
2015-10-03 23:13:53 +0200

    Extending timeout for browser (for TravisCI).

--- karma.conf.js
+++ karma.conf.js
@@ -21,6 +21,7 @@ module.exports = function (config) {
       type: 'lcov'
     },
     port: 9876,
+    browserNoActivityTimeout: 30000,
     colors: true,
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
