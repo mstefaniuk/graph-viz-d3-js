2920fa983b689094fa769ddbc3f561610cc9afce
mstefaniuk <marcin@stefaniuk.info>
2015-12-16 17:41:00 +0100

    Separating transitions from assigning attributes.

--- src/js/transitions/default.js
+++ src/js/transitions/default.js
@@ -1,90 +1,122 @@
 define([], function() {
   return {
-    document: function(svg, canvas) {
+    document: function(selection, canvas) {
       var area = [0, 0, canvas.width, canvas.height];
-      svg
+      var attributer = function() {
+        this
+          .attr("width", canvas.width + "pt")
+          .attr("height", canvas.height + "pt")
+          .attr("viewBox", area.join(' '))
+          .select("g")
+          .attr("transform", "scale(" + canvas.scaleWidth + " " + canvas.scaleHeight + ")" +
+            " " + "translate(" + canvas.vtranslate + "," + canvas.htranslate + ")");
+      };
+      selection
         .transition()
         .delay(150)
         .duration(700)
-        .attr("width", canvas.width + "pt")
-        .attr("height", canvas.height + "pt")
-        .attr("viewBox", area.join(' '))
-        .select("g")
-        .attr("transform", "scale(" + canvas.scaleWidth + " " + canvas.scaleHeight + ")" +
-          " " + "translate(" + canvas.vtranslate + "," + canvas.htranslate + ")");
+        .call(attributer);
     },
     canvas: function (svg, canvas) {
       var polygon = svg.select("polygon");
+      var attributer = function() {
+        this
+          .attr("points", function () {
+            return [[0,0],[0,canvas.height],[canvas.width,canvas.height],[canvas.width,0]]
+              .map(function (e) {
+                return e.join(",");
+              }).join(" ");
+          });
+        var self = this;
+        canvas.style.forEach(function(e) {
+          self.style(e.key, e.value);
+        });
+      };
       polygon
         .transition()
         .delay(150)
         .duration(900)
-        .attr("points", function () {
-          return [[0,0],[0,canvas.height],[canvas.width,canvas.height],[canvas.width,0]]
-            .map(function (e) {
-              return e.join(",");
-            }).join(" ");
-        });
-      canvas.style.forEach(function(e) {
-        polygon.style(e.key, e.value);
-      });
+        .call(attributer);
     },
-    nodes: function (nodes) {
-      nodes.style("opacity", 0.0)
-        .transition()
-        .delay(150)
-        .duration(900)
-        .style("opacity", 1.0);
-    },
-    relations: function (relations) {
-      relations.style("opacity", 0.0)
-        .transition()
-        .delay(150)
-        .duration(900)
-        .style("opacity", 1.0);
-    },
-    exits: function (exits) {
-      exits.transition()
-        .duration(100)
+    nodes: function (selection) {
+      var attributer = function() {
+        this
+          .style("opacity", 1.0);
+      };
+      selection
         .style("opacity", 0.0)
-        .remove();
+        .transition()
+        .delay(150)
+        .duration(900)
+        .call(attributer);
+    },
+    relations: function (selection) {
+      var attributer = function() {
+        this
+          .style("opacity", 1.0);
+      };
+      selection
+        .style("opacity", 0.0)
+        .transition()
+        .delay(150)
+        .duration(900)
+        .call(attributer);
+    },
+    exits: function (selection) {
+      var attributer = function() {
+        this
+          .style("opacity", 0.0)
+          .remove();
+      };
+      selection
+        .transition()
+        .duration(100)
+        .call(attributer);
     },
     shapes: function (shapes, palette) {
+      var attributer = function() {
+        this
+          .attr("d", function (d) {
+            var shape = d.shape;
+            return palette[shape](d);
+          })
+          .attr("style", function (d) {
+            return d.style.map(function (e) {
+              return [e.key, e.value].join(':');
+            }).join(';');
+          });
+      };
       shapes
         .transition()
         .delay(150)
         .duration(900)
-        .attr("d", function (d) {
-          var shape = d.shape;
-          return palette[shape](d);
-        })
-        .attr("style", function (d) {
-          return d.style.map(function (e) {
-            return [e.key, e.value].join(':');
-          }).join(';');
-        });
+        .call(attributer);
     },
     labels: function (labels) {
+      var attributer = function() {
+        this
+          .attr("x", function (d) {
+            return d.x;
+          })
+          .attr("y", function (d) {
+            return -d.y;
+          })
+          //.attr("text-anchor","middle")
+          //.attr("style", function(d) {
+          //  return d.style.map(
+          //    function(e){
+          //      return [e.key,e.value].join(':');
+          //    }).join(';');
+          //})
+          .text(function (d) {
+            return d.text;
+          });
+      };
       labels
         .transition()
         .delay(150)
         .duration(900)
-        .attr("x", function (d) {
-          return d.x;
-        })
-        .attr("y", function (d) {
-          return -d.y;
-        })
-        //.attr("text-anchor","middle")
-        //.attr("style", function(d) {
-        //  return d.style.map(
-        //    function(e){
-        //      return [e.key,e.value].join(':');
-        //    }).join(';');
-        //})
-        .text(function (d) {
-          return d.text;
-        });
+        .call(attributer);
     }
   };
 });
