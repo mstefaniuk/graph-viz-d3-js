aef5ecd2d8cb5c9228bacd6280bca0a3367ed942
mstefaniuk <marcin@stefaniuk.info>
2017-06-17 13:57:18 +0200

    Increasing browser timeout.

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function (config) {
       reportName: 'report'
     },
     port: 9876,
-    browserNoActivityTimeout: 30000,
+    browserNoActivityTimeout: 60000,
     colors: true,
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
