a1e1c53d0196a8357173c2e712d04c46a2d647a3
Magnus Jacobsson <magjac@netinsight.net>
2017-06-25 20:43:05 +0200

    Added test for quote handling in node label

---
+++ spec/asts/quote.js
@@ -0,0 +1,129 @@
+define({
+  "type": "digraph",
+  "id": "G",
+  "commands": [
+    {
+      "type": "graph",
+      "attributes": [
+        {
+          "type": "draw",
+          "elements": [
+            {
+              "shape": "polygon",
+              "points": [
+                [
+                  0,
+                  0
+                ],
+                [
+                  0,
+                  36
+                ],
+                [
+                  129.32,
+                  36
+                ],
+                [
+                  129.32,
+                  0
+                ]
+              ],
+              "style": [
+                {
+                  "key": "stroke",
+                  "value": "#fffffe00"
+                },
+                {
+                  "key": "fill",
+                  "value": "#ffffff"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "name": "bb",
+          "type": "skip"
+        },
+        {
+          "name": "xdotversion",
+          "type": "skip"
+        }
+      ]
+    },
+    {
+      "type": "skip",
+      "attributes": [
+        {
+          "name": "label",
+          "type": "skip"
+        }
+      ]
+    },
+    {
+      "type": "node",
+      "id": "Test",
+      "attributes": [
+        {
+          "type": "draw",
+          "elements": [
+            {
+              "shape": "ellipse",
+              "cx": 64.66,
+              "cy": 18,
+              "rx": 64.82,
+              "ry": 18,
+              "style": [
+                {
+                  "key": "stroke",
+                  "value": "#000000"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "type": "ldraw",
+          "elements": [
+            {
+              "x": 64.66,
+              "y": 13.8,
+              "text": "hello\"world\"",
+              "anchor": "middle",
+              "style": [
+                {
+                  "key": "font-family",
+                  "value": "'Times-Roman',serif"
+                },
+                {
+                  "key": "font-size",
+                  "value": 14
+                },
+                {
+                  "key": "stroke",
+                  "value": "#000000"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "name": "height",
+          "type": "skip"
+        },
+        {
+          "name": "label",
+          "type": "skip"
+        },
+        {
+          "name": "pos",
+          "type": "skip"
+        },
+        {
+          "name": "width",
+          "type": "skip"
+        }
+      ]
+    }
+  ]
+});
