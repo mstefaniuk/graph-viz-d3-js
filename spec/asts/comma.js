2bfc14f41a562c9524af6574365d7520577e27d6
Magnus Jacobsson <magjac@netinsight.net>
2017-06-26 10:54:42 +0200

    Added test for comma in HTML-like node label

---
+++ spec/asts/comma.js
@@ -0,0 +1,129 @@
+define({
+  "type": "digraph",
+  "id": "G",
+  "commands": [
+    {
+      "type": "graph",
+      "attributes": [
+        {
+          "type": "draw",
+          "elements": [
+            {
+              "shape": "polygon",
+              "points": [
+                [
+                  0,
+                  0
+                ],
+                [
+                  0,
+                  36
+                ],
+                [
+                  118,
+                  36
+                ],
+                [
+                  118,
+                  0
+                ]
+              ],
+              "style": [
+                {
+                  "key": "stroke",
+                  "value": "#fffffe00"
+                },
+                {
+                  "key": "fill",
+                  "value": "#ffffff"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "name": "bb",
+          "type": "skip"
+        },
+        {
+          "name": "xdotversion",
+          "type": "skip"
+        }
+      ]
+    },
+    {
+      "type": "skip",
+      "attributes": [
+        {
+          "name": "label",
+          "type": "skip"
+        }
+      ]
+    },
+    {
+      "type": "node",
+      "id": "Test",
+      "attributes": [
+        {
+          "type": "draw",
+          "elements": [
+            {
+              "shape": "ellipse",
+              "cx": 59,
+              "cy": 18,
+              "rx": 59.19,
+              "ry": 18,
+              "style": [
+                {
+                  "key": "stroke",
+                  "value": "#000000"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "type": "ldraw",
+          "elements": [
+            {
+              "x": 21,
+              "y": 14.3,
+              "text": "first,second",
+              "anchor": "start",
+              "style": [
+                {
+                  "key": "font-family",
+                  "value": "'Times-Roman',serif"
+                },
+                {
+                  "key": "font-size",
+                  "value": 14
+                },
+                {
+                  "key": "stroke",
+                  "value": "#000000"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "name": "height",
+          "type": "skip"
+        },
+        {
+          "name": "label",
+          "type": "skip"
+        },
+        {
+          "name": "pos",
+          "type": "skip"
+        },
+        {
+          "name": "width",
+          "type": "skip"
+        }
+      ]
+    }
+  ]
+});
